8d367ecf7327f459f80c1f7c474fe9a45b608ebf
fajar <124814792+fajarnas@users.noreply.github.com>
2025-06-15 17:14:44 +0700

    Update ig.py

--- ig.py
+++ ig.py
@@ -2,14 +2,11 @@ import pywhatkit as kit
 import time
 
 # Daftar nomor yang akan dikirim pesan
-nomor_list = ["+628567122478", "+6285882088077"]
+nomor_list = ["+62", "+62"]
 
 # Pesan yang akan dikirim
 pesan = (
     "Halo, ini adalah pesan otomatis yang dikirim menggunakan Python. "
-    "Maaf malam-malam ganggu. PPT sudah dibuat, kalau ada tambahan boleh ditambahkan, "
-    "atau kalau ada yang direvisi, silakan direvisi: "
-    "https://docs.google.com/presentation/d/1GkjDG4_4gwr-9NFXieYPCILj8xweQuPwlmEIrzlm-ZY/edit?usp=sharing\n"
     "Terima kasih!"
 )
 
